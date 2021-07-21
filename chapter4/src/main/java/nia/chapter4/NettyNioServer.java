f8240ec491183dffec74a9c60cdc2ed8f96ea7cb
Kevin_T <596823919@qq.com>
2021-07-21 19:11:19 +0800

    chore: format chapter4

--- chapter4/src/main/java/nia/chapter4/NettyNioServer.java
+++ chapter4/src/main/java/nia/chapter4/NettyNioServer.java
@@ -17,32 +17,28 @@ import java.nio.charset.Charset;
  * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
  */
 public class NettyNioServer {
+
     public void server(int port) throws Exception {
-        final ByteBuf buf =
-                Unpooled.unreleasableBuffer(Unpooled.copiedBuffer("Hi!\r\n",
-                        Charset.forName("UTF-8")));
+        final ByteBuf buf = Unpooled.unreleasableBuffer(Unpooled.copiedBuffer("Hi!\r\n", Charset.forName("UTF-8")));
         NioEventLoopGroup group = new NioEventLoopGroup();
         try {
             ServerBootstrap b = new ServerBootstrap();
-            b.group(group).channel(NioServerSocketChannel.class)
+            b.group(group)
+                    .channel(NioServerSocketChannel.class)
                     .localAddress(new InetSocketAddress(port))
                     .childHandler(new ChannelInitializer<SocketChannel>() {
-                                      @Override
-                                      public void initChannel(SocketChannel ch)
-                                              throws Exception {
-                                              ch.pipeline().addLast(
-                                                  new ChannelInboundHandlerAdapter() {
-                                                      @Override
-                                                      public void channelActive(
-                                                              ChannelHandlerContext ctx) throws Exception {
-                                                                ctx.writeAndFlush(buf.duplicate())
-                                                                  .addListener(
-                                                                          ChannelFutureListener.CLOSE);
-                                                      }
-                                                  });
-                                      }
-                                  }
-                    );
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(
+                                    new ChannelInboundHandlerAdapter() {
+                                        @Override
+                                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                            ctx.writeAndFlush(buf.duplicate())
+                                                    .addListener(ChannelFutureListener.CLOSE);
+                                        }
+                                    });
+                        }
+                    });
             ChannelFuture f = b.bind().sync();
             f.channel().closeFuture().sync();
         } finally {
--- chapter4/src/main/java/nia/chapter4/NettyOioServer.java
+++ chapter4/src/main/java/nia/chapter4/NettyOioServer.java
@@ -17,10 +17,9 @@ import java.nio.charset.Charset;
  * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
  */
 public class NettyOioServer {
-    public void server(int port)
-            throws Exception {
-        final ByteBuf buf =
-                Unpooled.unreleasableBuffer(Unpooled.copiedBuffer("Hi!\r\n", Charset.forName("UTF-8")));
+
+    public void server(int port) throws Exception {
+        final ByteBuf buf = Unpooled.unreleasableBuffer(Unpooled.copiedBuffer("Hi!\r\n", Charset.forName("UTF-8")));
         EventLoopGroup group = new OioEventLoopGroup();
         try {
             ServerBootstrap b = new ServerBootstrap();
@@ -29,17 +28,13 @@ public class NettyOioServer {
                     .localAddress(new InetSocketAddress(port))
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
-                        public void initChannel(SocketChannel ch)
-                                throws Exception {
-                                ch.pipeline().addLast(
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(
                                     new ChannelInboundHandlerAdapter() {
                                         @Override
-                                        public void channelActive(
-                                                ChannelHandlerContext ctx)
-                                                throws Exception {
+                                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
                                             ctx.writeAndFlush(buf.duplicate())
-                                                    .addListener(
-                                                            ChannelFutureListener.CLOSE);
+                                                    .addListener(ChannelFutureListener.CLOSE);
                                         }
                                     });
                         }
